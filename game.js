3bc7161476d810e6a995c7cea41a52402fb1ae27
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2020-02-02 11:10:15 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,47 @@
 // 0.0.2
 
 let WIDTH = 700, HEIGHT = 500;
-let buttonDimX = 200, buttonDimY = 150, screen = 0, time = 0;
+let buttonDimX = 165, buttonDimY = 47, screen = 0, time = 0;
 let timeStarted = Date.now(), menu = [];
 let onTransition = false;
-let backButton, img, transition, fps;
+let backButton, playImg, creditsImg, transition, fps, player, level, scrollX, scrollY;
+
+class Player {
+	update = () => {	
+		this.velY += 0.2;
+		this.posY += this.velY;
+		this.falling++;
+		
+		this.velX *= 0.82;	
+		this.posX += this.velX;
+		
+		while (!touchingGround()) {
+			this.posY -= 0.2;
+			this.velY = 0;
+			this.falling = 0;
+		}
+		
+		if (keyIsDown(LEFT_ARROW)) this.velX -= 1;
+		if (keyIsDown(RIGHT_ARROW)) this.velX += 1;
+		if (keyIsDown(UP_ARROW) && this.falling < 3) this.velY = -6;
+
+		if (this.posY > 500) reset();
+		scrollX = this.posX;
+	}
+
+	draw = () => {
+		fill(64);
+		rect(this.posX - scrollX + WIDTH / 2, this.posY, 30, 50);
+	}
+	
+	constructor(posX, posY) {
+		this.falling = 0;
+		this.velX = 0;
+		this.velY = 0;
+		this.posX = posX;
+		this.posY = posY;
+	}
+}
 
 function SceneTransition() {
 	this.y = -HEIGHT;
@@ -23,7 +60,7 @@ function SceneTransition() {
 		this.update();
 
 		fill(0);
-		rect(0, this.y, WIDTH, HEIGHT);
+		rect(WIDTH / 2, this.y, WIDTH, HEIGHT);
 	}
 }
 
@@ -58,6 +95,7 @@ function MenuItem(x_, y_, ySpeed_, yPower_, img_, id_) {
 	
 	this.clicked = () => {
 		if (this.id === 2) {
+			reset();
 			handleTransition(1);
 		} else if (this.id === 3) {
 			handleTransition(2)
@@ -68,18 +106,7 @@ function MenuItem(x_, y_, ySpeed_, yPower_, img_, id_) {
 	
 	this.update = () => {
 		this.xPos = (typeof this.x === "number") ? this.x : this.x.update();
-		
 		this.yPos = this.y + sin(frameCount * this.ySpeed) * this.yPower;
-		
-		if (mouseX >= this.xPos - this.dimX / 2 && mouseX <= this.xPos + this.dimX / 2 && mouseY >= this.y - this.dimY / 3 && mouseY <= this.yPos + this.dimY / 5) {
-			this.tint = true;
-			
-			if (mouseIsPressed) {
-				this.clicked();
-			}
-		} else {
-			if (this.tint) this.tint = false;
-		}
 	}
 	
 	this.render = () => {
@@ -93,6 +120,16 @@ function MenuItem(x_, y_, ySpeed_, yPower_, img_, id_) {
 		imageMode(CENTER);
 		image(this.img, 0, 0, 0, 0);
 		pop();
+		
+		if (mouseX >= this.xPos - this.dimX / 2 && mouseX <= this.xPos + this.dimX / 2 && mouseY >= this.y - this.dimY / 3 && mouseY <= this.yPos + this.dimY / 5) {
+			this.tint = true;
+			
+			if (mouseIsPressed) {
+				this.clicked();
+			}
+		} else {
+			if (this.tint) this.tint = false;
+		}
 	}
 }
 
@@ -117,38 +154,61 @@ makeButtonTransition = (pos, delay) => {
 }
 
 preload = () => {
-	img = loadImage("https://cors-anywhere.herokuapp.com/http://pixelartmaker.com/art/9b4cf54d4690c1d.png");
+	playImg = loadImage("https://cors-anywhere.herokuapp.com/https://eternaifire.github.io/game/play.svg");
+	creditsImg = loadImage("https://cors-anywhere.herokuapp.com/https://eternaifire.github.io/game/credits.svg");
 }
 
 setup = () => {
-	menu.push(new MenuItem(makeButtonTransition(WIDTH / 2, 0), 100, 0.05, 10, img, 1));
-	menu.push(new MenuItem(makeButtonTransition(WIDTH / 2, 500), 250, 0.05, 10, img, 2));
-	menu.push(new MenuItem(makeButtonTransition(WIDTH / 2, 1000), 350, 0.05, 10, img, 3));
-	backButton = new MenuItem(WIDTH / 2, 50, 0.05, 10, img, 4);	
+	reset();
+	noStroke();
+	rectMode(CENTER);
+	menu.push(new MenuItem(makeButtonTransition(WIDTH / 2, 0), 100, 0.05, 10, playImg, 1));
+	menu.push(new MenuItem(makeButtonTransition(WIDTH / 2, 500), 250, 0.05, 10, playImg, 2));
+	menu.push(new MenuItem(makeButtonTransition(WIDTH / 2, 1000), 350, 0.05, 10, creditsImg, 3));
+	backButton = new MenuItem(WIDTH / 2, 50, 0.05, 10, playImg, 4);	
 	createCanvas(WIDTH, HEIGHT);
 }
 
+reset = () => {
+	scrollX = 0;
+	scrollY = 0;
+	player = new Player(0, -50);
+}
+
+renderGame = () => {
+	player.update();
+	drawGround();
+	player.draw();
+}
+
+drawGround = () => {
+	fill(255);
+	rect(WIDTH / 2 - scrollX, 350 - scrollY, 500, 50);
+}
+
+touchingGround = () => {
+	return player.posY <= 300;
+}
+
 draw = () => {
-	background(250);
-	
+	background(52, 166, 251);
+
 	menu.forEach((item) => {	
-		if (screen == 0) {
+		if (screen === 0) {
 			item.render();
 		} else {
 			item.update();
 		}
 	});
 	
-	if (screen == 1) {
+	if (screen === 1) {
+		renderGame();
 		backButton.render();
+	} else if (screen === 2) {
 		textAlign(CENTER);
 		textSize(50);
-		text("GAME", WIDTH / 2, HEIGHT / 2);
-	} else if (screen == 2) {
+		text("MADE BY BONFIRE\n AND ETERNITY", WIDTH / 2, HEIGHT / 2);
 		backButton.render();
-		textAlign(CENTER);
-		textSize(50);
-		text("CREDITS", WIDTH / 2, HEIGHT / 2);
 	}
 	
 	if (transition) transition.render();
